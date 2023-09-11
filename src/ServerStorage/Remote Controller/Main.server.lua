cb1e224592c490d551eefa8b71a5028432074bd5
Martin <shkafchik.game@gmail.com>
2023-09-11 19:38:25 +0530

    init

--- src/ServerStorage/Remote Controller/Main.server.lua
+++ src/ServerStorage/Remote Controller/Main.server.lua
@@ -1,61 +1,46 @@
 local folder = script.Parent
-local gui = folder:WaitForChild("RemoteControllerGui"):Clone()
--- frame that will contain all remote events/functions in the game
-local scrollingFrame = gui:WaitForChild("ScrollingFrame")
+local widgetInfo = DockWidgetPluginGuiInfo.new(Enum.InitialDockState.Float, false, false, 200, 50, 100, 30)
+local widget = plugin:CreateDockWidgetPluginGui("Remote Controller", widgetInfo)
+widget.Title = "0 REMOTES SELECTED"
+widget.AutoLocalize = false
+
 -- a bar that will accept user input
-local argsBar = gui:WaitForChild("ArgsBar")
--- a table where each text button is binded to a remote event/function
-local buttons = {} -- TextButton = RemoteEvent
+local argsBar: TextBox = folder:WaitForChild("ArgsBar"):Clone()
+argsBar.Parent = widget
 
-local function UpdateCanvasSize(Canvas, Constraint)
-	Canvas.CanvasSize = UDim2.new(0, Constraint.AbsoluteContentSize.X, 0, Constraint.AbsoluteContentSize.Y + 20)
-end
+local toolbar = plugin:CreateToolbar("martytyty2098")
+local button = toolbar:CreateButton(
+	"Remote Controller",
+	"Super power to trigger remote events and functions at will", -- Trigger a remote event/function during play test
+	"rbxassetid://14749933281"
+)
 
-scrollingFrame.ChildAdded:Connect(function()
-	UpdateCanvasSize(scrollingFrame, scrollingFrame.UIGridLayout)
+button.Click:Connect(function()
+	widget.Enabled = not widget.Enabled
 end)
 
--- create a button for every remote event/function in the game and bind them together inside a table
+widget.Changed:Connect(function(property)
+	if property == "Enabled" then
+		button.Enabled = false
+		button.Enabled = true
+	end
+end)
+
+-- find all remote events/functions
 for _, obj in pairs(game:GetDescendants()) do
 	if obj:IsA("RemoteEvent") then
-		local button = gui.ExampleButton:Clone()
-		button.Text = obj.Name
-		button.Parent = scrollingFrame
-		buttons[button] = obj
 	end
 end
 
 local selectedButton: TextButton = nil
 
-for currButton: TextButton, remote in pairs(buttons) do
-	currButton.Activated:Connect(function()
-		if selectedButton then
-			selectedButton.BackgroundTransparency = 1
-		end
-		selectedButton = currButton
-		selectedButton.BackgroundTransparency = 0
-		argsBar.Visible = true
-	end)
-	currButton.MouseEnter:Connect(function()
-		if selectedButton == currButton then
-			return
-		end
-		currButton.BackgroundTransparency = 0.5
-	end)
-	currButton.MouseLeave:Connect(function()
-		if selectedButton == currButton then
-			return
-		end
-		currButton.BackgroundTransparency = 1
-	end)
-end
-
 argsBar.FocusLost:Connect(function(enterPressed: boolean)
 	if not enterPressed then
 		return
 	end
+
 	local input = argsBar.ContentText
-	local values = gui.SendLoadStringRequest:InvokeServer(input)
+	local values = loadstring("return " .. "{" .. input .. "}")() -- pcall
 	for i, v in pairs(values) do
 		print(i, v, typeof(v))
 	end
