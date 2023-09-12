39f7aee7fb3d4e18ecc77428510b04d5a7ef7048
Martin <shkafchik.game@gmail.com>
2023-09-12 16:14:40 +0530

    init

--- src/ServerStorage/Remote Controller/Main.server.lua
+++ src/ServerStorage/Remote Controller/Main.server.lua
@@ -1,7 +1,8 @@
 local folder = script.Parent
+local selection = game:GetService("Selection")
 local widgetInfo = DockWidgetPluginGuiInfo.new(Enum.InitialDockState.Float, false, false, 200, 50, 100, 30)
 local widget = plugin:CreateDockWidgetPluginGui("Remote Controller", widgetInfo)
-widget.Title = "0 REMOTES SELECTED"
+widget.Title = "0 EVENTS AND 0 FUNCTIONS SELECTED"
 widget.AutoLocalize = false
 
 -- a bar that will accept user input
@@ -11,7 +12,7 @@ argsBar.Parent = widget
 local toolbar = plugin:CreateToolbar("martytyty2098")
 local button = toolbar:CreateButton(
 	"Remote Controller",
-	"Super power to trigger remote events and functions at will", -- Trigger a remote event/function during play test
+	"Super power to trigger remote events and functions at will",
 	"rbxassetid://14749933281"
 )
 
@@ -23,16 +24,37 @@ widget.Changed:Connect(function(property)
 	if property == "Enabled" then
 		button.Enabled = false
 		button.Enabled = true
+		if widget.Enabled then
+			button:SetActive(true)
+		else
+			button:SetActive(false)
+		end
 	end
 end)
 
--- find all remote events/functions
-for _, obj in pairs(game:GetDescendants()) do
-	if obj:IsA("RemoteEvent") then
-	end
-end
+local selectedEvents: { RemoteEvent } = {}
+local selectedFunctions: { RemoteFunction } = {}
+local eventAmount = 0
+local functionAmount = 0
 
-local selectedButton: TextButton = nil
+-- get all selected remote events/functions
+selection.SelectionChanged:Connect(function()
+	table.clear(selectedEvents)
+	table.clear(selectedFunctions)
+	eventAmount = 0
+	functionAmount = 0
+	local selected = selection:Get()
+	for _, obj in pairs(selected) do
+		if obj.ClassName == "RemoteEvent" then
+			table.insert(selectedEvents, obj)
+			eventAmount += 1
+		elseif obj.ClassName == "RemoteFunction" then
+			table.insert(selectedFunctions, obj)
+			functionAmount += 1
+		end
+	end
+	widget.Title = string.format("%u EVENTS AND %u FUNCTIONS SELECTED", eventAmount, functionAmount)
+end)
 
 argsBar.FocusLost:Connect(function(enterPressed: boolean)
 	if not enterPressed then
@@ -43,5 +65,13 @@ argsBar.FocusLost:Connect(function(enterPressed: boolean)
 	local values = loadstring("return " .. "{" .. input .. "}")() -- pcall
 	for i, v in pairs(values) do
 		print(i, v, typeof(v))
+	end
+	print("Events:")
+	for i, v in pairs(selectedEvents) do
+		print(i, v)
+	end
+	print("Functions:")
+	for i, v in pairs(selectedFunctions) do
+		print(i, v)
 	end
 end)
